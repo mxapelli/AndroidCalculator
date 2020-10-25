57a7047a9bda8bdcb4b11893a8e3c2c834c50e6b
mxapelli <72257485+mxapelli@users.noreply.github.com>
2020-10-25 21:05:06 +0100

    AndroidCalculatorGood

--- app/src/main/java/edu/upc/androidcalculator/MainActivity.java
+++ app/src/main/java/edu/upc/androidcalculator/MainActivity.java
@@ -11,12 +11,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
      TextView Resultado;
+     double numbers[];
+     int num = 0;
+     String operations[];
+     String currentNum;
+     int radianes;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Resultado = (TextView)findViewById(R.id.Etiqueta);
+        Resultado = findViewById(R.id.Etiqueta);
+        numbers = new double[10];
+        operations = new String[10];
+        currentNum="";
+        radianes=1;
 
     }
 
@@ -39,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         mostrar("6");
     }
     public void pressButton7(View view) {
+
         mostrar("7");
     }
     public void pressButton8(View view) {
@@ -48,44 +59,163 @@ public class MainActivity extends AppCompatActivity {
         mostrar("9");
     }
     public void pressButton0(View view) {
+
         mostrar("0");
     }
     //Operaciones
     public void pressButtonSuma(View view) {
+        numbers[num] = Double.parseDouble(currentNum);
+        operations[num] = "+";
+        num +=1;
         mostrar("+");
+        currentNum="";
     }
     public void pressButtonResta(View view) {
+        numbers[num] = Double.parseDouble(currentNum);
+        operations[num] = "-";
+        num +=1;
         mostrar("-");
-    }
-    public void pressButtonDiv(View view) {
-        mostrar("/");
+        currentNum="";
     }
     public void pressButtonMult(View view) {
+        numbers[num] = Double.parseDouble(currentNum);
+        operations[num] = "x";
+        num +=1;
         mostrar("x");
+        currentNum="";
+    }
+    public void pressButtonDiv(View view) {
+        numbers[num] = Double.parseDouble(currentNum);
+        operations[num] = "/";
+        num +=1;
+        mostrar("/");
+        currentNum="";
+    }
+    public void pressButtonIgual(View view) {
+        numbers[num] = Double.parseDouble(currentNum);
+        int i;
+        double currentResult = 0;
+        if (operations[0] == "+")
+            currentResult = numbers[0] + numbers[1];
+        else if (operations[0] == "-")
+            currentResult = numbers[0] - numbers[1];
+        else if (operations[0] == "x")
+            currentResult = numbers[0] * numbers[1];
+        else if (operations[0] == "/")
+            currentResult = numbers[0] / numbers[1];
+        else if (operations[0] == "cos")
+        {
+            if (radianes==0) {
+                numbers[0]=numbers[0]*Math.PI/180;
+            }
+            currentResult = Math.cos(numbers[0]);
+        }
+        else if (operations[0] == "sin")
+        {
+            if (radianes==0) {
+                numbers[0]=numbers[0]*Math.PI/180;
+            }
+            currentResult = Math.sin(numbers[0]);
+        }
+        else if (operations[0] == "tan")
+        {
+            if (radianes==0) {
+                numbers[0]=numbers[0]*Math.PI/180;
+            }
+            currentResult = Math.tan(numbers[0]);
+        }
+
+        for (i=1; i < num; i++) {
+            if (operations[i] == "+")
+                currentResult +=  numbers[i+1];
+            else if (operations[i] == "-")
+                currentResult -= numbers[i+1];
+            else if (operations[i] == "x")
+                currentResult *= numbers[i+1];
+            else if (operations[i] == "/")
+                currentResult /= numbers[i+1];
+        }
+        Resultado.setText(Double.toString(currentResult));
+        num = 0;
+        numbers = new double[15];
+        operations = new String[15];
+        currentNum="";
+
     }
     //Trigonometria
     public void pressButtonCoseno(View view) {
+        if (currentNum.equals("") && numbers[0]==0){
+            Resultado.setText("cos");
+            operations[num] = "cos";
+        }
+        else{
+            num = 0;
+            numbers = new double[15];
+            operations = new String[15];
+            Resultado.setText("ERROR");
+            currentNum="";
 
+        }
     }
     public void pressButtonSeno(View view) {
+        if (currentNum.equals("") && numbers[0]==0){
+            Resultado.setText("sin");
+            operations[num] = "sin";
+        }
+        else{
+            num = 0;
+            numbers = new double[15];
+            operations = new String[15];
+            Resultado.setText("ERROR");
+            currentNum="";
+
+        }
+
 
     }
     public void pressButtonTangente(View view) {
+        System.out.println(numbers[0]);
+        if (currentNum.equals("") && numbers[0]==0){
+            Resultado.setText("tan");
+            operations[num] = "tan";
+        }
+        else{
+            num = 0;
+            numbers = new double[15];
+            operations = new String[15];
+            Resultado.setText("ERROR");
+            currentNum="";
 
-    }
-    //Punto
-    public void pressButtonPunto(View view) {
-
-    }
-    public void pressButtonBorrar(View view) {
+        }
 
     }
     public void pressButtonClean(View view) {
-        mostrar("5");
+        num = 0;
+        numbers = new double[15];
+        operations = new String[15];
+        currentNum="";
+        Resultado.setText("");
     }
     public void mostrar(String res){
-        Resultado.setText(Resultado.getText()+res);
+        if (currentNum.equals("") && numbers[0]==0 || Resultado.getText().equals("ERROR"))
+        {
+            Resultado.setText(res);
+        }
+        else{
+            Resultado.setText(Resultado.getText() + res);
+        }
+        currentNum=currentNum+res;
     }
+    public void pressButtonGrados(View view){
+        radianes=0;
+    }
+    public void pressButtonRadianes(View view){
+        radianes=1;
+
+    }
+
+
+
 }
 
 
